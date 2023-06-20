f93f5c70a40122f414ca14138031eba203f6279e
Ailin Venerus <ailinvenerus@gmail.com>
2023-06-20 11:06:36 +0100

    Hotfix: updating script (#10)

--- bin/auth.ts
+++ bin/auth.ts
@@ -6,9 +6,9 @@ import { program } from 'commander';
 program
   .command('token')
   .description('Get all the JWTs (id, access and refresh) given a user/password pair')
-  .arguments('<clientid> <username> <password>')
-  .action(async (clientid: string, username: string, password: string) => {
-    const auth = new Auth({ clientId: clientid });
+  .arguments('<clientid> <userpoolid> <username> <password>')
+  .action(async (clientid: string, userpoolid: string, username: string, password: string) => {
+    const auth = new Auth({ clientId: clientid, userPoolId: userpoolid });
     const tokens = await auth.getTokens({ username, password });
     console.info(tokens);
   });
@@ -16,9 +16,9 @@ program
 program
   .command('credentials')
   .description('Get the credentials using an id token')
-  .arguments('<userpoolid> <fedid> <token>')
-  .action(async (userpoolid: string, fedid: string, token: string) => {
-    const auth = new Auth({ userPoolId: userpoolid, identityPoolId: fedid });
+  .arguments('<clientid> <userpoolid> <fedid> <token>')
+  .action(async (clientid: string, userpoolid: string, fedid: string, token: string) => {
+    const auth = new Auth({ clientId: clientid, userPoolId: userpoolid, identityPoolId: fedid });
     const fedUserId = await auth.getFederatedId(token);
     const credentials = await auth.getFederatedCredentials(fedUserId, token);
     console.info(credentials);
