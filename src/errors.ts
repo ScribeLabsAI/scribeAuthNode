722874c67731a0929f1ab1cc86aacc83317290ff
ailinvenerus <ailinvenerus@gmail.com>
2023-12-21 11:17:28 +0000

    feat: add automatic doc generation and update pipeline

--- src/errors.ts
+++ src/errors.ts
@@ -1,11 +1,11 @@
+/**
+Exception raised when a user cannot perform an action.
+Possible reasons:
+Username and/or Password are incorrect.
+Refresh token is incorrect.
+MFA code is incorrect or expired.
+*/
 export class UnauthorizedError extends Error {
-  /**
-  Exception raised when a user cannot perform an action.
-  Possible reasons:
-  Username and/or Password are incorrect.
-  Refresh token is incorrect.
-  MFA code is incorrect or expired.
-  */
   constructor(message: string, cause?: Error) {
     super(message);
     this.name = 'UnauthorizedError';
@@ -13,13 +13,13 @@ export class UnauthorizedError extends Error {
   }
 }
 
+/**
+Exception raised when an action is performed by a user too many times in a short period.
+Actions that could raise this exception:
+Changing a password.
+Revoking a refresh token.
+*/
 export class TooManyRequestsError extends Error {
-  /**
-  Exception raised when an action is performed by a user too many times in a short period.
-  Actions that could raise this exception:
-  Changing a password.
-  Revoking a refresh token.
-  */
   constructor(message: string, cause?: Error) {
     super(message);
     this.name = 'TooManyRequestsError';
@@ -48,14 +48,14 @@ export class MissingFieldError extends Error {
   }
 }
 
+/**
+ * Exception raised when MFA fails.
+ * Actions that could raise this exception:
+ * A code has expired.
+ * The provided code doesn't match what the server was expecting.
+ *
+ */
 export class MFAError extends Error {
-  /**
-   * Exception raised when MFA fails.
-   * Actions that could raise this exception:
-   * A code has expired.
-   * The provided code doesn't match what the server was expecting.
-   *
-   */
   constructor(message: string) {
     super(message);
     this.name = 'MFAError';
